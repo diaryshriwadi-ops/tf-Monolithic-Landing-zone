9826c48eab1f28750749cd1036519de48e9e08e9
diaryshriwadi-ops <diaryshriwadi@gmail.com>
2026-07-27 22:51:21 +0530

    Delete modules directory

--- modules/azurerm_pip/main.tf
+++
@@ -1,9 +0,0 @@
-variable "pipinfo" {}
-resource "azurerm_public_ip" "pip" {
-    for_each = var.pipinfo
-    name = each.value.name
-    resource_group_name = each.value.resource_group_name
-    location = each.value.location
-    allocation_method = "Static"
-  
-}
--- modules/azurerm_resource_group/main.tf
+++
@@ -1,7 +0,0 @@
-variable "rginfo" {}
-resource "azurerm_resource_group" "rg" {
-    for_each = var.rginfo
-    name=each.value.name
-    location=each.value.location
-  
-}
